3a4d659b7411252b590b6c65469de1829b627e2f
Ricky <thechaosmyth7@gmail.com>
2025-10-08 23:51:27 +0700

    feat: added image illustration on homepage

--- src/shared/views/HomeView.vue
+++ src/shared/views/HomeView.vue
@@ -2,6 +2,10 @@
 import { IconArrowNarrowRight } from "@tabler/icons-vue"
 import { onMounted, ref } from "vue"
 
+import HomepageImage from "@/assets/images/homepage.png"
+
+import { GeneratePageName } from "@/features/generate/models"
+
 const showContent = ref(false)
 const showButton = ref(false)
 
@@ -25,7 +29,8 @@ onMounted(() => {
         enter-from-class="opacity-0 translate-y-10"
         enter-to-class="opacity-100 translate-y-0"
       >
-        <div v-if="showContent" class="space-y-3">
+        <div v-if="showContent" class="space-y-3 flex flex-col items-center justify-center">
+          <img :src="HomepageImage" alt="homepage" class="h-60 object-cover" />
           <h1 class="text-3xl md:text-5xl font-bold md:font-extrabold tracking-tight">
             Find Your Next Adventure
           </h1>
@@ -43,13 +48,17 @@ onMounted(() => {
           enter-from-class="opacity-0 translate-y-4 scale-95"
           enter-to-class="opacity-100 translate-y-0 scale-100"
         >
-          <button
-            v-if="showButton"
-            class="cursor-pointer group bg-[#00FF87] text-black font-semibold px-6 py-2 rounded-md shadow-lg flex items-center gap-2 hover:bg-[#00E67A] hover:scale-[1.03] active:scale-95 transition-all duration-300 focus:outline-none focus:ring-2 focus:ring-[#00FF87]/50"
-          >
-            <span>Generate Activity</span>
-            <IconArrowNarrowRight class="w-5 h-5 text-black animate-slideRight" stroke-width="2" />
-          </button>
+          <RouterLink v-if="showButton" :to="{ name: GeneratePageName.GENERATE }">
+            <button
+              class="cursor-pointer group bg-[#00FF87] text-black font-semibold px-6 py-2 rounded-md shadow-lg flex items-center gap-2 hover:bg-[#00E67A] hover:scale-[1.03] active:scale-95 transition-all duration-300 focus:outline-none focus:ring-2 focus:ring-[#00FF87]/50"
+            >
+              <span>Generate Activity</span>
+              <IconArrowNarrowRight
+                class="w-5 h-5 text-black animate-slideRight"
+                stroke-width="2"
+              />
+            </button>
+          </RouterLink>
         </transition>
       </div>
     </div>
